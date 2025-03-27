da9ac3330aa1a40f1984d95cb9cfe5661994ed5c
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-03-26 23:43:48 -0300

    feat: Add Cors Settings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes/index');
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
+
+const corsOptions = {
+    credentials: true,   
+    optionSuccessStatus: 200,
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+};
+app.use(cors(corsOptions));
+
 app.use(routes);
 
 console.log('Server started!');
