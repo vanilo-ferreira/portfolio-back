dc44be0362c111bee400b4744d8ca256829248c3
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-04-03 12:42:04 -0300

    fix: Update endpoint

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const corsOptions = {
     credentials: true,   
     optionSuccessStatus: 200,
-    origin: "http://localhost:5173",
+    origin: "https://portfolio-services-mzhd.onrender.com",
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
 };
