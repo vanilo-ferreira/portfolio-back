955f408111da4ada40c38fdac290fbe94c6ada0f
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-05-06 20:48:11 -0300

    fix: Update endpoint of origin variable

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes/index');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes/index");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
 const corsOptions = {
-    credentials: true,   
-    optionSuccessStatus: 200,
-    origin: "https://portfolio-m8sotfec1-vaniloferreiras-projects.vercel.app",
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
+  credentials: true,
+  optionSuccessStatus: 200,
+  origin: "https://portfolio-one-flame-10.vercel.app/",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
 };
 app.use(cors(corsOptions));
 
 app.use(routes);
 
-console.log('Server started!');
+console.log("Server started!");
 
 app.listen(8000);
